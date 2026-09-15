350bbc33637e65132e8d9cab33c76d09fa5fe922
biggestmanuel <emmanuelnwuba1@gmail.com>
2026-09-15 01:02:15 +0100

    Fix PostgreSQL connection startup

--- vorth-backend/server.js
+++ vorth-backend/server.js
@@ -1,5 +1,5 @@
 const env = require('./src/config/env');
-const connectDB = require('./src/config/db');
+const { connectDB } = require('./src/config/db');
 const app = require('./src/app');
 const registerJobs = require('./src/jobs/resetViews');
 
